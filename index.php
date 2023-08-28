98379bf2bd51c4f6d3a5f8ff91f0076382bc5100
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2023-08-28 09:18:05 +0700

    upload new branch

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-        <title>Home</title>
+        <title>Home 123456</title>
     </head>
     <body>
         <div class="container">
